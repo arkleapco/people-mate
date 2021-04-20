82af9177ff853dfabe144f1e00c533da42276805
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-04-20 11:57:45 +0200

    Sqlite

--- MashreqPayroll/settings/development.py
+++ MashreqPayroll/settings/development.py
@@ -6,21 +6,21 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#      }
-#  }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'people_mate',
-        'USER': 'mashreq_sysadmin',
-        'PASSWORD': 'M@$hreq123',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'people_mate',
+#         'USER': 'mashreq_sysadmin',
+#         'PASSWORD': 'M@$hreq123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
