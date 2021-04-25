93258466ea6f54a68704c1445aa830f46dbc0381
jihadfathalla <jihadfathalla3@gmail.com>
2021-04-25 13:16:52 +0200

    fix

--- performance/views.py
+++ performance/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse , reverse
 from django.db import IntegrityError
 from django.utils import translation
