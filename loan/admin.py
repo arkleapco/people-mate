8f3ec4bbd854486d2bb59635bf61c54ac97cd13e
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-10-24 11:41:59 +0200

    add enterprise to loan admin

--- loan/admin.py
+++ loan/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Loan)
-admin.site.register(LoanType)
+
+class LoanTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company')
+admin.site.register(LoanType , LoanTypeAdmin)
 admin.site.register(LoanInstallment)
