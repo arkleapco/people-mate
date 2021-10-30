af8a06b37221044669b85039557d2215db83f908
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-10-30 15:01:35 +0200

    Update new_tax_rules.py

--- payroll_run/new_tax_rules.py
+++ payroll_run/new_tax_rules.py
@@ -1,3 +1,4 @@
+from django.db.models.aggregates import Sum
 from defenition.models import TaxRule, Tax_Sections
 
 
@@ -37,7 +38,7 @@ class Tax_Deduction_Amount:
                 tax_values += tax_amount_for_section
         print("Tax sections here >>> ", employee_sections)
         print("Tax sections here >>> ", tax_list)
-        return tax_values
+        return Sum(tax_list)
 
     def _tax_calaulation(self, annual_tax_salary):
         # هل المرتب اكثر من 600 الف ؟
