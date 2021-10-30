f49ad05ea44f6425181494f6ee540940069427a7
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-10-30 13:22:41 +0200

    Update salary_calculations.py

--- payroll_run/salary_calculations.py
+++ payroll_run/salary_calculations.py
@@ -195,8 +195,8 @@ class Salary_Calculator:
     def calc_net_salary(self):
         # taxes_and_insurance=  self.calc_taxes_deduction() + (self.calc_emp_deductions_amount() + self.calc_employee_insurance())
         # net_salary = self.calc_gross_salary() - taxes_and_insurance
-        net_salary = self.calc_gross_salary() - self.calc_taxes_deduction() - self.calc_employee_insurance() - self.calc_emp_deductions_amount()
-
+        net_salary = self.calc_gross_salary() - ( self.calc_taxes_deduction() +  self.calc_employee_insurance() + self.calc_emp_deductions_amount())
+        print("Here is net salary >> ", net_salary)
         return net_salary
 
 
