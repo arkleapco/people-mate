76ff412e1adcbd199e0380b699116a90901aa420
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-11-10 11:03:48 +0200

    Update models.py

--- company/models.py
+++ company/models.py
@@ -34,13 +34,10 @@ class Enterprise(models.Model):
         Country, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('Country'))
     slug = models.SlugField(blank=True, null=True)
     start_date = models.DateField(
-        auto_now=False, auto_now_add=False, default=date.today, verbose_name=_('Start Date'))
-
-    oracle_erp_id = models.CharField(max_length=100, null=True, blank=True,  verbose_name=_('Oracle ID'))
-    
+        auto_now=False, auto_now_add=False, default=date.today, verbose_name=_('Start Date'))    
     end_date = models.DateField(
         auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name=_('End Date'))
-
+    oracle_erp_id = models.CharField(max_length=100, null=True, blank=True,  verbose_name=_('Oracle ID'))
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, on_delete=models.CASCADE,
                                    related_name="company_created_by")
     creation_date = models.DateField(auto_now=True, auto_now_add=False)
