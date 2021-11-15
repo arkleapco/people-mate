e8a8ca7354c780018652ef7b6c895dd414dc78b7
jihadfathalla <jihadfathalla3@gmail.com>
2021-11-15 13:28:10 +0200

    import data from orcale api

--- company/api/urls.py
+++ company/api/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     ######################### API URLs ###################################
     path('list/company', views.list_company, name='list-company'),
     path('list/department', views.list_department, name='import-department'),
-    # path('list/job', views.list_job, name='import-job'),   
-    # path('list/grade', views.list_grade, name='import-grade'),   
-    # path('list/position', views.list_position, name='import-position'),   
+    path('list/job', views.list_job, name='import-job'),   
+    path('list/grade', views.list_grade, name='import-grade'),   
+    path('list/position', views.list_position, name='import-position'),   
 
    
     
