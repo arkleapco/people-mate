57e39b2ecf5f55cc7b8bbb36ccc3ca56eb9d7b4d
ArkLeap <93133635+arkleapco@users.noreply.github.com>
2021-11-28 15:52:57 +0200

    Update models.py

--- payroll_run/models.py
+++ payroll_run/models.py
@@ -269,7 +269,7 @@ class EmployeePayrollElements4(models.Model):
 
 @receiver(pre_save, sender=Salary_elements)
 def employee_elements_history(sender, instance, *args, **kwargs):
-    employee_old_elements = Employee_Element.objects.filter(emp_id=instance.emp)
+    employee_old_elements = Employee_Element.objects.filter(emp_id=instance.emp, element_id__classification__code='earn')
     check_for_same_element = Employee_Element_History.objects.filter(emp_id=instance.emp_id,
                                                                      salary_month=instance.salary_month,
                                          
