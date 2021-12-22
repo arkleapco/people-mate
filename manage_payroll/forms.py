6ad0e22ebe74a7ed395b9464630443ce5ff57a01
jihadfathalla <jihadfathalla3@gmail.com>
2021-12-22 11:54:55 +0200

    make choose date when terminate employee and edit include and exclude employee query in batch

--- manage_payroll/forms.py
+++ manage_payroll/forms.py
@@ -100,12 +100,13 @@ class AssignmentBatchIncludeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
 
-        emp_salry_structure = EmployeeStructureLink.objects.filter(salary_structure__enterprise=user.company,end_date__isnull=True).values_list("employee", flat=True)
+        # emp_salry_structure = EmployeeStructureLink.objects.filter(salary_structure__enterprise=user.company,end_date__isnull=True).values_list("employee", flat=True)
         super(AssignmentBatchIncludeForm, self).__init__(*args, **kwargs)
         self.fields['dept_id'].queryset = Department.objects.filter(end_date__isnull=True, enterprise=user.company)
         self.fields['position_id'].queryset = Position.objects.filter(end_date__isnull=True, department__enterprise=user.company)
         self.fields['job_id'].queryset = Job.objects.filter(end_date__isnull=True, enterprise=user.company)
-        self.fields['emp_id'].queryset = Employee.objects.filter(id__in=emp_salry_structure,emp_end_date__isnull=True, enterprise=user.company)
+        # self.fields['emp_id'].queryset = Employee.objects.filter(id__in=emp_salry_structure,emp_end_date__isnull=True, enterprise=user.company)
+        self.fields['emp_id'].queryset = Employee.objects.filter(enterprise=user.company ).filter(Q(emp_end_date__gte=date.today()) | Q(emp_end_date__isnull=True))
         self.fields['start_date'].widget.input_type = 'date'
         self.fields['end_date'].widget.input_type = 'date'
         for field in self.fields:
@@ -129,13 +130,14 @@ class AssignmentBatchExcludeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
-        emp_salry_structure = EmployeeStructureLink.objects.filter(salary_structure__enterprise=user.company,end_date__isnull=True).values_list("employee", flat=True)
+        # emp_salry_structure = EmployeeStructureLink.objects.filter(salary_structure__enterprise=user.company,end_date__isnull=True).values_list("employee", flat=True)
         super(AssignmentBatchExcludeForm, self).__init__(*args, **kwargs)
         self.fields['dept_id'].queryset = Department.objects.filter(end_date__isnull=True, enterprise=user.company)
         self.fields['position_id'].queryset = Position.objects.filter(end_date__isnull=True,
                                                                       department__enterprise=user.company)
         self.fields['job_id'].queryset = Job.objects.filter(end_date__isnull=True, enterprise=user.company)
-        self.fields['emp_id'].queryset = Employee.objects.filter(id__in = emp_salry_structure,emp_end_date__isnull=True, enterprise=user.company)
+        # self.fields['emp_id'].queryset = Employee.objects.filter(id__in = emp_salry_structure,emp_end_date__isnull=True, enterprise=user.company)
+        self.fields['emp_id'].queryset = Employee.objects.filter(enterprise=user.company ).filter(Q(emp_end_date__gte=date.today()) | Q(emp_end_date__isnull=True))
         self.fields['start_date'].widget.input_type = 'date'
         self.fields['end_date'].widget.input_type = 'date'
         for field in self.fields:
